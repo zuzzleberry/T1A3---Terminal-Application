e3d7d8192860ea207242fc8f5608d9fa2da8f54f
Adrian Zuanic <azuanic@gmail.com>
2022-04-19 16:31:26 +1000

    started basic budget structure

---
+++ lib/budget_maker.rb
@@ -0,0 +1,46 @@
+require 'colorize'
+
+
+
+
+# Function to calculate monthly budget
+def monthly_budget(name)
+    puts "Hi #{name}! Let's calculate your monthly budget.".green
+    puts "Please enter your monthly take home (after tax) income:".green
+    income = gets.chomp.to_i
+    # Calculate Mandatory Expenses
+    puts "Please enter your monthly rent:".green
+    rent = gets.chomp.to_i
+    puts "Please enter your monthly bills:".green
+    bills = gets.chomp.to_i
+    puts "Please enter your monthly groceries:".green
+    groceries = gets.chomp.to_i
+    puts "Please enter your monthly debts:".green
+    debts = gets.chomp.to_i
+    puts "Please enter your monthly transport costs:".green
+    transport = gets.chomp.to_i
+    puts "Please enter your monthly entertainment spending (movies, nightclubs etc):".green
+    entertainment = gets.chomp.to_i
+    puts "Please enter your monthly personal spending (clothes, haircuts etc):".green
+    personal = gets.chomp.to_i
+    puts "Please enter your monthly other miscellanious spending:".green
+    other = gets.chomp.to_i
+end
+
+# Function to display monthly budget
+def display_budget(name, income, rent, bills, groceries, debts, transport, entertainment, personal, other)
+    puts "Hi #{name}! Here is your monthly budget:".green
+    puts "Monthly take home income: $#{income}".green
+    puts "Monthly rent: $#{rent}".green
+    puts "Monthly bills: $#{bills}".green
+    puts "Monthly groceries: $#{groceries}".green
+    puts "Monthly debts: $#{debts}".green
+    puts "Monthly transport costs: $#{transport}".green
+    puts "Monthly entertainment spending: $#{entertainment}".green
+    puts "Monthly personal spending: $#{personal}".green
+    puts "Monthly other miscellanious spending: $#{other}".green
+    puts "Monthly budget: $#{income - rent - bills - groceries - debts - transport - entertainment - personal - other}".green
+end
+
+
+
--- lib/terminal_app.rb
+++ lib/terminal_app.rb
@@ -1,8 +1,12 @@
-# frozen_string_literal: true
-
+require 'colorize'
 require_relative "terminal_app/version"
+require_relative "budget_maker"
 
-module TerminalApp
-  class Error < StandardError; end
-  # Your code goes here...
-end
+puts "Welcome to Budget maker!".green
+
+puts "Let's calculate your budget.".green
+puts "Please enter your name:".green
+name = gets.chomp
+
+monthly_budget(name)
+display_budget(name, income, rent, bills, groceries, debts, transport, entertainment, personal, other)
